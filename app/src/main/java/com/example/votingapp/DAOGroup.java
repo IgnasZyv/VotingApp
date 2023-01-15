999a6cb920a03dd75bc643b155f6718ba85810d4
Ignas <Ignaszyv@gmail.com>
2023-01-15 15:09:18 +0000

    Log in functionality added. Fixed minor bugs

--- app/src/main/java/com/example/votingapp/DAOGroup.java
+++ app/src/main/java/com/example/votingapp/DAOGroup.java
@@ -1,18 +1,21 @@
 package com.example.votingapp;
 
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 public class DAOGroup {
     private final DatabaseReference mDatabase;
 
     public DAOGroup() {
         FirebaseDatabase db = FirebaseDatabase.getInstance("https://votingapp-6e7b7-default-rtdb.europe-west1.firebasedatabase.app/");
-        mDatabase = db.getReference(Group.class.getSimpleName());
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        mDatabase = db.getReference(Group.class.getSimpleName()).child(Objects.requireNonNull(auth.getUid()));
     }
 
     public Task<Void> add(Group group) {
--- app/src/main/java/com/example/votingapp/DAOQuestion.java
+++ app/src/main/java/com/example/votingapp/DAOQuestion.java
@@ -1,17 +1,20 @@
 package com.example.votingapp;
 
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 public class DAOQuestion {
     private final DatabaseReference mDatabase;
 
     public DAOQuestion() {
         FirebaseDatabase db = FirebaseDatabase.getInstance("https://votingapp-6e7b7-default-rtdb.europe-west1.firebasedatabase.app/");
-        mDatabase = db.getReference(Group.class.getSimpleName());
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        mDatabase = db.getReference(Group.class.getSimpleName()).child(Objects.requireNonNull(auth.getUid()));
     }
 
     public Task<Void> add(Question question, String groupId) {
